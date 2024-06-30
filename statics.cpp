4134a4645e84c312fba11fb806db44ab675cc2cf
ASMIT YADAV <asmityadav3@gmail.com>
2024-06-30 18:33:30 +0530

    second change

--- statics.cpp
+++ statics.cpp
@@ -22,10 +22,16 @@ public:
 int A::b = 0;
 int main()
 {
-    A obj(10, 20);
-    A obj1(10, 200);
-    obj.disp();
-    A::disp();
-    obj.show();
-    return 0;
+    // A obj(10, 20);
+    // A obj1(10, 200);
+    // obj.disp();
+    // A::disp();
+    // obj.show();
+    // return 0;
+
+   const int x=10;
+  // x = 10;
+   printf("%d", x);
+   return 0;
+
 }
